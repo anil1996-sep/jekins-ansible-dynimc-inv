fa37ad73f67d86374278c6001a0537568e22c4a5
anil1996-sep <107744913+anil1996-sep@users.noreply.github.com>
2024-05-28 09:29:14 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,14 +2,12 @@ provider "aws"{
 region="ap-south-1"
 }
 
-
 resource "aws_instance" "AWSEC2Instance"{
 ami = "ami-0cc9838aa7ab1dce7"
 instance_type = "t2.micro"
-key_name = "devopsmssmay"
+key_name = "Keypair2024"
 security_groups = ["launch-wizard-2"]
 tags={
 Name = "terraformserver"
 }
 }
-
